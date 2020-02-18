59404dd555fda179c114ed5b970424ad472a787e
Christopher Santana <christopher.santana@nav.no>
2020-02-18 11:01:05 +0100

    Removed an API call. Added missing auth.

--- find_existing_workflows.sh
+++ find_existing_workflows.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
 ## Find version sha for existing files, if present
-GITHUB_FOLDER_SHA=$(curl -s -u "$API_ACCESS_TOKEN:" "https://api.github.com/repos/$REPOSITORY/git/trees/$BASE_TREE_SHA" | jq -r '.tree[] | select(.path == ".github").sha')
-
-if [[ -z $GITHUB_FOLDER_SHA ]]; then
-  echo "[]"
-  exit 0
-fi
-
-WORKFLOW_FOLDER_SHA=$(curl -s -u "$API_ACCESS_TOKEN:" "https://api.github.com/repos/$REPOSITORY/git/trees/$GITHUB_FOLDER_SHA" | jq -r '.tree[] | select(.path == "workflows").sha')
+WORKFLOW_FOLDER_SHA=$(curl -s -u "$API_ACCESS_TOKEN:" "https://api.github.com/repos/$REPOSITORY/git/trees/$BASE_TREE_SHA?recursive=1" | jq -r '.tree[] | select(.path == ".github/workflows").sha')
 
 if [[ -z $WORKFLOW_FOLDER_SHA ]]; then
   echo "[]"
   exit 0
 fi
 
-curl -s "https://api.github.com/repos/$REPOSITORY/git/trees/$WORKFLOW_FOLDER_SHA" | jq '.tree'
+curl -s -u "$API_ACCESS_TOKEN:" "https://api.github.com/repos/$REPOSITORY/git/trees/$WORKFLOW_FOLDER_SHA" | jq '.tree'
